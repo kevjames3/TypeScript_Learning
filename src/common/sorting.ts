dfc99e93a6653a28fcbc0cc4941df82e679ecf2d
Kevin McIntosh <kevjames3@gmail.com>
2016-11-21 08:05:21 -0800

    Established interface, flow, and type ref

--- src/common/sorting.ts
+++ src/common/sorting.ts
@@ -1,9 +1,11 @@
-export default class Sort {
-    static QuickSort(){
-
+/** The comparitor function for sorting */
+type Comparitor<F> = (a: F, b: F) => number;
+export default class Sort {    
+    public static QuickSort<T>(input:Array<T>, comparitor?:Comparitor<T>) : T[] {        
+        return input;
     }
 
-    static MergeSort(){
-        
+    public static MergeSort<T>(input:Array<T>, comparitor?:Comparitor<T>) : T[] {        
+        return input;
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,10 @@ import Sort from './common/sorting';
 
 let anArray:Array<number> = [];
 
-console.log("TypeScript"); 
+for(let i:number = 0; i < 10; i++){
+    anArray.push(Math.floor(Math.random() * 100));
+}
+Sort.QuickSort(anArray, (a: number, b: number) => {return a - b});
+Sort.MergeSort(anArray, (a: number, b: number) => {return a - b});
+
+console.log(anArray); 
