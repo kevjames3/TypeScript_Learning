4b69a12c4464dc2605cd939f0f5617b3361c72a2
Kevin McIntosh <kevjames3@gmail.com>
2016-11-21 22:25:53 -0800

    My attempt at QuickSort

--- src/common/sorting.ts
+++ src/common/sorting.ts
@@ -1,11 +1,38 @@
 /** The comparitor function for sorting */
 type Comparitor<F> = (a: F, b: F) => number;
 export default class Sort {    
-    public static QuickSort<T>(input:Array<T>, comparitor?:Comparitor<T>) : T[] {        
-        return input;
-    }
-
-    public static MergeSort<T>(input:Array<T>, comparitor?:Comparitor<T>) : T[] {        
-        return input;
+    public static MergeSort<T>(input:T[], comparitor:Comparitor<T>) : T[] {        
+        function Merge(a: T[], b: T[]) : T[]{
+            let newArray : T[] = [];
+            if (a.length > 0 && b.length == 0){
+                newArray = a;
+            } else if (b.length > 0 && a.length == 0){
+                newArray = b;
+            } else {
+                for(let i = 0, j = 0; i < a.length || j < b.length;){
+                    let comparison = comparitor(a[i], b[j]);
+                    if(comparison < 0){
+                        newArray.push(b[j]);
+                        j++;
+                    } else if (comparison == 0){
+                        newArray.push(a[i]);
+                        newArray.push(a[j]);
+                        i++ && j++;
+                    } else { //comparison > 0
+                        newArray.push(a[i]);
+                        i++;
+                    }
+                }
+            }
+            return newArray;
+        }
+                
+        if(input.length == 0){
+            return input;
+        }
+        let middle = Math.floor(input.length/2);
+        let sortA = Sort.MergeSort(input.slice(0, middle), comparitor);
+        let sortB = Sort.MergeSort(input.slice(middle, input.length), comparitor);
+        return Merge(sortA, sortB);
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,9 @@ let anArray:Array<number> = [];
 
 for(let i:number = 0; i < 10; i++){
     anArray.push(Math.floor(Math.random() * 100));
-}
-Sort.QuickSort(anArray, (a: number, b: number) => {return a - b});
-Sort.MergeSort(anArray, (a: number, b: number) => {return a - b});
+} 
+//Sort.QuickSort(anArray, (a: number, b: number) => {return a - b});
+let newArray = Sort.MergeSort(anArray, (a: number, b: number) => {return a - b});
 
-console.log(anArray); 
+console.log("Old Array: " + anArray);
+console.log("New Array: " + newArray); 
