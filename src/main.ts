b87466936215a8c89d249bb05f820cba694ab13f
Kevin McIntosh <kevjames3@gmail.com>
2016-12-04 21:57:49 -0800

    Changed the strings to use backticks

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,12 @@ import Sort from './common/sorting';
 
 let anArray:Array<number> = [];
 
-for(let i:number = 0; i < 10; i++){
-    anArray.push(Math.floor(Math.random() * 100));
+for(let i:number = 0; i < 25; i++){
+    anArray.push(Math.floor(Math.random() * 1000));
 } 
-//Sort.QuickSort(anArray, (a: number, b: number) => {return a - b});
-let newArray = Sort.MergeSort(anArray, (a: number, b: number) => {return a - b});
+let newQuickArray = Sort.QuickSort(anArray, (a: number, b: number) => {return a - b});
+let newMergeArray = Sort.MergeSort(anArray, (a: number, b: number) => {return a - b});
 
-console.log("Old Array: " + anArray);
-console.log("New Array: " + newArray); 
+console.log(`Old Array: ${anArray}`);
+console.log(`New Merge Sort Array: ${newMergeArray}`); 
+console.log(`New Quicksort Array ${newQuickArray}`);
